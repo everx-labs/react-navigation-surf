7596a4ba81bf14fead3ecb3dd4fdca909c306ee5
Aleksei Saveliev <savelichalex93@gmail.com>
2020-07-10 11:00:57 +0300

    Separate lib and example

--- src/SurfRouter.js
+++ src/SurfRouter.js
@@ -1,56 +1,197 @@
 // @flow strict-local
-import {nanoid} from 'nanoid/non-secure';
-import {BaseRouter, StackRouter, TabRouter} from '@react-navigation/native';
+import { nanoid } from 'nanoid/non-secure';
+import { BaseRouter, StackRouter, TabRouter } from '@react-navigation/native';
+import type {
+    RouterFactory,
+    NavigationState,
+    GenericNavigationAction,
+    TabRouterOptions,
+} from '@react-navigation/native';
 
-let isSplitted = true;
-export const setIsSplitted = s => {
-  isSplitted = s;
+export const SurfSplitActions = {
+    setSplitted(isSplitted: boolean, initialRouteName: string) {
+        return { type: 'SET_SPLITTED', isSplitted, initialRouteName };
+    },
 };
 
-export function SurfSplitRouter(options) {
-  const tabRouter = TabRouter(options);
-  const stackRouter = StackRouter(options);
-  const router = {
-    ...BaseRouter,
+const MAIN_SCREEN_NAME = 'main';
 
-    get type() {
-      return isSplitted ? tabRouter.type : stackRouter.type;
-    },
+type SurfRouterOptions = {|
+    ...TabRouterOptions,
+    isSplitted: boolean,
+|};
 
-    getInitialState(...args) {
-      return isSplitted
-        ? tabRouter.getInitialState(...args)
-        : stackRouter.getInitialState(...args);
-    },
+const stackStateToTab = (state, options: SurfRouterOptions) => {
+    let { index } = state;
 
-    getRehydratedState(...args) {
-      return isSplitted
-        ? tabRouter.getRehydratedState(...args)
-        : stackRouter.getRehydratedState(...args);
-    },
+    const currentRoute = state.routes[index];
+    if (currentRoute.name !== MAIN_SCREEN_NAME) {
+        index = state.routeNames.indexOf(currentRoute.name);
+    } else if (options.initialRouteName != null) {
+        index = state.routeNames.indexOf(options.initialRouteName);
+    }
 
-    getStateForRouteNamesChange(...args) {
-      return isSplitted
-        ? tabRouter.getStateForRouteNamesChange(...args)
-        : stackRouter.getStateForRouteNamesChange(...args);
-    },
+    const routes = state.routeNames.map(name => {
+        return (
+            state.routes.find(({ name: routeName }) => routeName === name) || {
+                name,
+                key: `${name}-${nanoid()}`,
+            }
+        );
+    });
 
-    getStateForRouteFocus(...args) {
-      return isSplitted
-        ? tabRouter.getStateForRouteFocus(...args)
-        : stackRouter.getStateForRouteFocus(...args);
-    },
+    return {
+        ...state,
+        index,
+        routes,
+        history: [],
+    };
+};
 
-    getStateForAction(...args) {
-      return isSplitted
-        ? tabRouter.getStateForAction(...args)
-        : stackRouter.getStateForAction(...args);
-    },
+const tabStateToStack = (state, options: SurfRouterOptions) => {
+    let { index } = state;
+    const mainRoute = state.routes.find(
+        ({ name }) => name === MAIN_SCREEN_NAME,
+    ) || {
+        name: MAIN_SCREEN_NAME,
+        key: `${MAIN_SCREEN_NAME}-${nanoid()}`,
+    };
+    let routes;
+    const currentRoute = state.routes[index];
+    if (currentRoute.name === MAIN_SCREEN_NAME) {
+        index = 0;
+        routes = [mainRoute];
+    } else {
+        index = 1;
+        routes = [mainRoute, currentRoute];
+    }
+    return {
+        ...state,
+        index,
+        routes,
+    };
+};
 
-    shouldActionChangeFocus(action) {
-      return tabRouter.shouldActionChangeFocus(action);
-    },
-  };
+export const SurfSplitRouter: RouterFactory<
+    NavigationState,
+    GenericNavigationAction,
+    SurfRouterOptions,
+> = routerOptions => {
+    let { isSplitted, ...tabOptions } = routerOptions;
+    const { backBehavior, ...stackOptions } = tabOptions;
+    const tabRouter = TabRouter(tabOptions);
+    const stackRouter = StackRouter(stackOptions);
+    let isInitialized = false;
+    let initialRouteName = routerOptions.initialRouteName;
+    const router = {
+        // $FlowExpectedError
+        ...BaseRouter,
 
-  return router;
-}
+        type: 'surf',
+
+        getInitialState(...args) {
+            const newState = isSplitted
+                ? tabRouter.getInitialState(...args)
+                : stackRouter.getInitialState(...args);
+
+            Object.assign(newState, { type: router.type });
+            return newState;
+        },
+
+        getRehydratedState(state, params) {
+            const isStale = state.stale;
+            let newState;
+
+            if (isStale === false) {
+                return state;
+            }
+
+            if (isSplitted) {
+                newState = tabRouter.getRehydratedState(state, params);
+
+                // Move from "main" route in splitted version
+                const currentRouteName = newState.routeNames[newState.index];
+                if (currentRouteName === MAIN_SCREEN_NAME) {
+                    if (initialRouteName != null) {
+                        newState.index = newState.routeNames.indexOf(
+                            routerOptions.initialRouteName,
+                        );
+                    } else {
+                        newState.index = newState.index + 1;
+                    }
+                }
+            } else {
+                newState = stackRouter.getRehydratedState(state, params);
+
+                const mainRoute = newState.routes.find(
+                    ({ name }) => name === MAIN_SCREEN_NAME,
+                ) || {
+                    name: MAIN_SCREEN_NAME,
+                    key: `${MAIN_SCREEN_NAME}-${nanoid()}`,
+                };
+                const currentRoute = newState.routes[newState.index];
+                if (currentRoute.name === MAIN_SCREEN_NAME) {
+                    newState.index = 0;
+                    newState.routes = [mainRoute];
+                } else {
+                    newState.index = 1;
+                    newState.routes = [mainRoute, currentRoute];
+                }
+            }
+
+            Object.assign(newState, { type: router.type });
+            return newState;
+        },
+
+        getStateForRouteNamesChange(...args) {
+            const newState = isSplitted
+                ? tabRouter.getStateForRouteNamesChange(...args)
+                : stackRouter.getStateForRouteNamesChange(...args);
+
+            Object.assign(newState, { type: router.type });
+            return newState;
+        },
+
+        getStateForRouteFocus(...args) {
+            const newState = isSplitted
+                ? tabRouter.getStateForRouteFocus(...args)
+                : stackRouter.getStateForRouteFocus(...args);
+
+            Object.assign(newState, { type: router.type });
+            return newState;
+        },
+
+        getStateForAction(state, action, options) {
+            if (action.type === 'SET_SPLITTED') {
+                isSplitted = action.isSplitted;
+                if (!isInitialized) {
+                    isInitialized = true;
+                    return state;
+                }
+
+                if (action.initialRouteName) {
+                    initialRouteName = action.initialRouteName;
+                }
+
+                if (isSplitted) {
+                    return stackStateToTab(state, routerOptions);
+                }
+                return tabStateToStack(state, routerOptions);
+            }
+
+            const newState = isSplitted
+                ? tabRouter.getStateForAction(state, action, options)
+                : stackRouter.getStateForAction(state, action, options);
+
+            // $FlowFixMe
+            Object.assign(newState, { type: router.type });
+            return newState;
+        },
+
+        shouldActionChangeFocus(action) {
+            return tabRouter.shouldActionChangeFocus(action);
+        },
+    };
+
+    return router;
+};
--- src/createSurfNavigator.js
+++ src/createSurfNavigator.js
@@ -1,137 +1,155 @@
 // @flow strict-local
 import * as React from 'react';
 import {
-  useWindowDimensions,
-  StyleSheet,
-  View,
-  Text,
-  Dimensions,
+    useWindowDimensions,
+    StyleSheet,
+    View,
+    Text,
+    Dimensions,
 } from 'react-native';
+import type { ViewStyleProp } from 'react-native/Libraries/StyleSheet/StyleSheet';
 import {
-  NavigationHelpersContext,
-  useNavigationBuilder,
-  createNavigatorFactory,
-  TabRouter,
+    NavigationHelpersContext,
+    useNavigationBuilder,
+    createNavigatorFactory,
+    TabRouter,
 } from '@react-navigation/native';
-import {StackView} from '@react-navigation/stack';
+import { StackView } from '@react-navigation/stack';
+// $FlowExpectedError
 import ResourceSavingScene from '@react-navigation/bottom-tabs/lib/module/views/ResourceSavingScene';
 
-import {SurfSplitRouter, setIsSplitted} from './SurfRouter';
+import { SurfSplitRouter, SurfSplitActions } from './SurfRouter';
 
-const tabStateToStackOne = ({history, ...rest}) => {
-  const {index} = rest;
-  const routes = [rest.routes[0]];
-  return {
-    ...rest,
-    type: 'stack',
-    routes: index > 0 ? routes.concat(rest.routes[index]) : routes,
-  };
-};
+const getIsSplitted = ({ width }, mainWidth) => width > mainWidth;
 
-const getIsSplitted = ({width}) => width > 600;
+type SurfSplitNavigatorProps = {|
+    +children?: React.Node,
+    initialRouteName: string,
+    mainWidth: number,
+    screenOptions: {
+        splitStyles?: {
+            body: ViewStyleProp,
+            main: ViewStyleProp,
+            detail: ViewStyleProp,
+        },
+        headerShown?: boolean,
+        ...
+    },
+|};
 
 export const SurfSplitNavigator = ({
-  children,
-  initialRouteName,
-  screenOptions,
-}) => {
-  const dimensions = useWindowDimensions();
-  const isSplitted = getIsSplitted(dimensions);
-  setIsSplitted(isSplitted);
+    children,
+    initialRouteName,
+    mainWidth,
+    screenOptions,
+}: SurfSplitNavigatorProps) => {
+    const dimensions = useWindowDimensions();
+    const isSplitted = getIsSplitted(dimensions, mainWidth);
 
-  let mainScreen;
-
-  const {state, navigation, descriptors} = useNavigationBuilder(
-    SurfSplitRouter,
-    {
-      children: isSplitted
-        ? React.Children.toArray(children).filter(child => {
-            if (child.props.name !== 'main') {
-              return true;
-            }
-            mainScreen = child.props.component;
-            return false;
-          })
-        : children,
-      initialRouteName: isSplitted ? initialRouteName : 'main',
-      screenOptions: {
-        ...screenOptions,
-        headerShown: false,
-      },
-    },
-  );
-
-  const loadedRef = React.useRef([]);
-
-  React.useEffect(() => {
-    if (!loadedRef.current.includes(state.index)) {
-      loadedRef.current = [...loadedRef.current, state.index];
-    }
-  }, [state]);
-
-  if (isSplitted) {
-    const MainScreen = mainScreen;
-    return (
-      <NavigationHelpersContext.Provider>
-        <View style={styles.body}>
-          <View style={styles.main}>
-            <MainScreen navigation={navigation} />
-          </View>
-          <View style={styles.detail}>
-            {state.routes.map((route, index) => {
-              const descriptor = descriptors[route.key];
-              const isFocused = state.index === index;
-
-              if (!isSplitted && !loadedRef.current.includes(index)) {
-                return null;
-              }
-
-              return (
-                <ResourceSavingScene
-                  key={route.key}
-                  style={StyleSheet.absoluteFill}
-                  isVisible={isFocused}>
-                  {descriptor.render()}
-                </ResourceSavingScene>
-              );
-            })}
-          </View>
-        </View>
-      </NavigationHelpersContext.Provider>
+    const {
+        splitStyles,
+        headerShown,
+        ...restScreenOptions
+    } = screenOptions || {
+        splitStyles: {
+            body: styles.body,
+            main: styles.main,
+            detail: styles.detail,
+        },
+    };
+    const { state, navigation, descriptors } = useNavigationBuilder(
+        SurfSplitRouter,
+        {
+            children,
+            initialRouteName: initialRouteName,
+            screenOptions: {
+                ...restScreenOptions,
+                headerShown: false,
+            },
+            isSplitted,
+        },
     );
-  }
-  return (
-    <StackView
-      state={state}
-      navigation={navigation}
-      descriptors={descriptors}
-    />
-  );
+
+    React.useEffect(() => {
+        navigation.dispatch(
+            SurfSplitActions.setSplitted(
+                isSplitted,
+                isSplitted ? initialRouteName : 'main',
+            ),
+        );
+    }, [isSplitted]);
+
+    const loadedRef = React.useRef([]);
+
+    React.useEffect(() => {
+        if (!loadedRef.current.includes(state.index)) {
+            loadedRef.current = [...loadedRef.current, state.index];
+        }
+    }, [state]);
+
+    if (isSplitted) {
+        const mainRoute = state.routes.find(({ name }) => name === 'main');
+        return (
+            <NavigationHelpersContext.Provider>
+                <View style={splitStyles.body}>
+                    <View style={splitStyles.main}>
+                        {descriptors[mainRoute.key].render()}
+                    </View>
+                    <View style={splitStyles.detail}>
+                        {state.routes.map((route, index) => {
+                            const descriptor = descriptors[route.key];
+                            const isFocused = state.index === index;
+
+                            if (
+                                !isSplitted &&
+                                !loadedRef.current.includes(index)
+                            ) {
+                                return null;
+                            }
+
+                            return (
+                                <ResourceSavingScene
+                                    key={route.key}
+                                    style={StyleSheet.absoluteFill}
+                                    isVisible={isFocused}
+                                >
+                                    {descriptor.render()}
+                                </ResourceSavingScene>
+                            );
+                        })}
+                    </View>
+                </View>
+            </NavigationHelpersContext.Provider>
+        );
+    }
+    return (
+        <StackView
+            state={state}
+            navigation={navigation}
+            descriptors={descriptors}
+        />
+    );
 };
 
 export const createSurfSplitNavigator = createNavigatorFactory(
-  SurfSplitNavigator,
+    SurfSplitNavigator,
 );
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'row',
-    padding: 10,
-  },
-  main: {
-    backgroundColor: 'white',
-    minWidth: 300,
-    marginRight: 10,
-    borderRadius: 5,
-  },
-  detail: {
-    backgroundColor: 'white',
-    flex: 1,
-    borderRadius: 5,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
+    body: {
+        flex: 1,
+        flexDirection: 'row',
+        padding: 10,
+    },
+    main: {
+        backgroundColor: 'white',
+        minWidth: 300,
+        marginRight: 10,
+        borderRadius: 5,
+    },
+    detail: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+    },
 });
