2d7e6debfd0a132cbe1d3bf0bd014a80261b96cf
Aleksei Saveliev <savelichalex93@gmail.com>
2020-07-10 11:31:20 +0300

    Remove flow-typed from git

--- Example/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
-    printWidth: 80,
-    tabWidth: 4,
-};
