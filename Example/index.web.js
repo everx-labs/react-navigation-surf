eac44e77281e32da359afc547948a7ee692aee4f
Aleksei Saveliev <savelichalex93@gmail.com>
2020-07-10 12:47:33 +0300

    Fixes to make it installable in example

--- Example/index.web.js
+++ Example/index.web.js
@@ -2,12 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
 AppRegistry.runApplication(appName, {
-  initialProps: {},
-  rootTag: document.getElementById(appName + 'Container'),
+    initialProps: {},
+    rootTag: document.getElementById(appName + 'Container'),
 });
